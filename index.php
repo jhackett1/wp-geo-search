df24be649a9de8d1f43b53540f90eea7d9eca59d
Jaye Hackett <joshua.hackett93@gmail.com>
2020-11-14 12:21:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,10 @@ Plugin name: WP Geo Search
 Description: Add a location-aware geographic search to WP_Query for use in your templates
 Version: 0.1
 Requires at least: 4
-Requires PHP: 5
+Requires PHP: 7
 Author: Jaye Hackett
 Author URI: https://jayehackett.com
 Plugin URI: https://github.com/jhackett1/wp-geo-search
-License: Apache License Version 2.0
 Text Domain: wp-geo-search
 */
 
